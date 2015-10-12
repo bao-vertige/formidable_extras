4c5f29b23a440abece523f0da00685231d6132c6
Michel Bystranowski <michel@vertige.org>
2015-10-12 12:08:47 +0200

    debug traitement formidable d'inscription

--- traiter/newsletters.php
+++ traiter/newsletters.php
@@ -8,10 +8,12 @@ function traiter_newsletters_dist($args, $retours){
     $options = $args['options'];
 
     $adresse_mail = _request($options['champ_adresse_inscrite']);
-    $listes = $options['listes_de_diffusion'];
-
-    $listes = _request('listes_de_diffusion_1');
+    $listes = _request($options['listes_de_diffusion']);
 
     $subscribe = charger_fonction('subscribe', 'newsletter');
     $subscribe($adresse_mail, array('listes' => $listes));
+
+	// noter qu'on a deja fait le boulot, pour ne pas risquer double appel
+	$retours['traitements']['newsletter'] = true;
+	return $retours;
 }
